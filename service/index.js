5cc014a7e9d760ebfd46c672b79e907b786ab49a
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2019-10-20 22:38:50 +0530

    updating

--- service/index.js
+++ service/index.js
@@ -1,47 +1,52 @@
 var utils = require('utils');
-var serand = require('serand');
 
 var makes;
+var makesById = {};
+var modelsById = {};
 
-var findOne = function (id, makes) {
-    var i;
-    var make;
-    var length = makes.length;
-    for (i = 0; i < length; i++) {
-        make = makes[i];
-        if (make.id === id) {
-            break;
-        }
-    }
-    return make;
-};
-
-exports.findOne = function (id, done) {
-    if (makes) {
-        return done(null, findOne(id, makes));
-    }
-    exports.find(function (err, makes) {
+exports.findModel = function (id, done) {
+    exports.find(function (err) {
         if (err) {
             return done(err);
         }
-        done(null, findOne(id, makes));
+        done(null, modelsById[id]);
     });
 };
 
-module.exports.find = function (done) {
+exports.findModels = function (id, done) {
+    exports.findOne(id, function (err, make) {
+        if (err) {
+            return done(err);
+        }
+        done(null, make.models);
+    });
+};
+
+exports.findOne = function (id, done) {
+    exports.find(function (err) {
+        if (err) {
+            return done(err);
+        }
+        done(null, makesById[id]);
+    });
+};
+
+exports.find = function (done) {
     if (makes) {
         return done(null, makes);
     }
-    utils.sync('vehicle-makes:find', function (ran) {
-        var next = utils.resolve('autos:///apis/v/vehicle-makes' + utils.data({
-            count: 100
-        }));
-        utils.all(next, function (err, o) {
-            if (err) {
-                return ran(err);
-            }
-            makes = _.sortBy(o, 'title');
-            ran(null, makes);
+    utils.configs('vehicle-makes', function (err, m) {
+        if (err) {
+            return done(err);
+        }
+        makes = m;
+        m.forEach(function (make) {
+            makesById[make.id] = make;
+            var models = make.models;
+            models.forEach(function (model) {
+                modelsById[model.id] = model;
+            });
         });
-    }, done);
+        done(null, makes);
+    });
 };
